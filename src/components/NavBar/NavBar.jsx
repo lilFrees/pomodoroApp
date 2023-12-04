728c798e7c90f0a1f137198e70dc511c9dd5b6bd
lilFrees <lil.trader@mail.ru>
2023-12-04 15:12:08 +0500

    fixes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,16 @@
 import style from "./NavBar.module.css";
 import { CgMenuLeftAlt } from "react-icons/cg";
 import Button from "../Button/Button";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { TimerContext } from "../../contexts/TimerContext";
 import { UserContext } from "../../contexts/UserContext";
 import { motion } from "framer-motion";
-import NavMenu from "./NavMenu";
 
 function NavBar() {
   const { switchTimerType, currentTimer, isActive, timeRemaining } =
     useContext(TimerContext);
   const { username } = useContext(UserContext);
+  const navRef = useRef();
 
   const [timerCount, setTimerCount] = useState([0, 0, 0]);
   const [isOpen, setIsOpen] = useState(false);
@@ -21,6 +21,24 @@ function NavBar() {
   function closeHandler() {
     setIsOpen(false);
   }
+
+  useEffect(() => {
+    const navClick = function (e) {
+      if (e.target === navRef.current && !isOpen) {
+        openHandler();
+        console.log("inside");
+      } else {
+        closeHandler();
+        console.log("outside");
+      }
+    };
+
+    window.addEventListener("click", navClick);
+
+    return () => {
+      window.removeEventListener("click", navClick);
+    };
+  }, []);
 
   useEffect(() => {
     const timers = JSON.parse(localStorage.getItem("timers"));
@@ -79,17 +97,37 @@ function NavBar() {
           {timerCount[2]} long breaks
         </Button>
       </div>
-      <Button
-        type="button"
-        alpha="1"
-        className={style.navToggleBtn}
-        onClick={openHandler}
-      >
-        <CgMenuLeftAlt />
-      </Button>
-      {isOpen && <NavMenu onClose={closeHandler} />}
+      <div ref={navRef}>
+        <Button
+          type="button"
+          alpha="1"
+          className={style.navToggleBtn}
+          // onClick={openHandler}
+        >
+          <CgMenuLeftAlt />
+        </Button>
+      </div>
+      {isOpen && (
+        <motion.div
+          layout
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={spring}
+          className={style.navMenu}
+        >
+          <div className={style.actions}></div>
+          <ul className={style.list}></ul>
+        </motion.div>
+      )}
     </div>
   );
 }
+
+const spring = {
+  type: "spring",
+  stiffness: 700,
+  damping: 50,
+};
 
 export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import ThemesProvider from "./Themes/Themes.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemesProvider>
-    <App />
-  </ThemesProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
